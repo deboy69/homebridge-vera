30681e6de4bcec2a6ee8444a4d718d566107a9ce
deboy69 <66630421+deboy69@users.noreply.github.com>
2023-12-02 13:04:48 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,47 @@
+index.js
+
 var Service, Characteristic, Accessory, uuid;
 
-var Veraconfig      = {};
-var debug           = require("debug")('VeraLink');
-var request         = require("request-promise");
-var hashing         = require("create-hash");
+var Veraconfig = {};
+var debug = require("debug")('VeraLink');
+var request = require("request-promise");
+var hashing = require("create-hash");
 
-module.exports = function (homebridge)
-{
-    if(typeof homebridge !== "undefined")
-    {
-        Service         = homebridge.hap.Service;
-        Characteristic  = homebridge.hap.Characteristic;
-        Accessory       = homebridge.hap.Accessory;
-        uuid            = homebridge.hap.uuid;
+module.exports = function (homebridge) {
+    if (typeof homebridge !== "undefined") {
+        Service = homebridge.hap.Service;
+        Characteristic = homebridge.hap.Characteristic;
+        Accessory = homebridge.hap.Accessory;
+        uuid = homebridge.hap.uuid;
         console.log("VeraLink initializing");
         homebridge.registerPlatform("homebridge-veralink", "Vera", VeraLinkPlatform);
     }
 };
 
 VeraLinkPlatform.prototype = {
-    accessories: function(callback)
-    {
-        this
-          .functions
-          .getVeraInfo()
-          .then( (verainfo)=>{
-            if(typeof verainfo === 'object'){
-              var devices = this.functions.processall(verainfo);
-              var accessories = devices.map( (device)=>{
-                  return this.createAccessory(device,this);
-              });
-              callback(accessories);
-            }
-          }
-        );
+    accessories: function (callback) {
+        this.functions.getVeraInfo()
+            .then((verainfo) => {
+                if (typeof verainfo === 'object') {
+                    var devices = this.functions.processall(verainfo);
+                    var accessories = devices.map((device) => {
+                        return this.createAccessory(device, this);
+                    });
+                    callback(accessories);
+                }
+            })
+            .catch((error) => {
+                console.error('Error getting Vera info:', error);
+                callback([]);
+            });
     },
-    createAccessory: function(device,platform) {
+    createAccessory: function (device, platform) {
         var properties = new Object({
-          platform: platform,
-          name: device.displayName,
-          getServices : function(){
-            return this.services;
-          }
+            platform: platform,
+            name: device.displayName,
+            getServices: function () {
+                return this.services;
+            }
         });
 
         Object.assign(device, properties);
@@ -50,64 +50,60 @@ VeraLinkPlatform.prototype = {
     }
 };
 
-function VeraLinkPlatform(log, config)
-{
-    var Veraconfig  = loadconfig();
-    this.log        = log;
-    this.rooms      = {};
-    this.HAPNode     = {'request':request, 'uuid':uuid, 'Accessory':Accessory, 'Service':Service, 'Characteristic':Characteristic, 'debug':debug, 'hashing':hashing, 'return': true};
+function VeraLinkPlatform(log, config) {
+    var Veraconfig = loadconfig();
+    this.log = log;
+    this.rooms = {};
+    this.HAPNode = { 'request': request, 'uuid': uuid, 'Accessory': Accessory, 'Service': Service, 'Characteristic': Characteristic, 'debug': debug, 'hashing': hashing, 'return': true };
 
     process.on('uncaughtException', function (err) {
         debug(err);
     });
 
     defaults = {
-      bridged: true,
-      includesensor: false,
-      dimmertest: false,
-      ignorerooms: [],
-      ignoredevices: [],
-      securitypoll: 2000
+        bridged: true,
+        includesensor: false,
+        dimmertest: false,
+        ignorerooms: [],
+        ignoredevices: [],
+        securitypoll: 2000
     };
 
     Veraconfig = merge_options(defaults, Veraconfig);
-    Veraconfig = merge_options(Veraconfig,config);
+    Veraconfig = merge_options(Veraconfig, config);
 
-    if(typeof config.veraIP === "undefined")
-    {
+    if (typeof config.veraIP === "undefined") {
         console.log("\033[31m No configuration found, please write your configuration on .homebridge/config.json \033[0m");
-        console.log("\033[31m or add your configuration file to "+home+"/.veralink/config.js \033[0m");
+        console.log("\033[31m or add your configuration file to " + home + "/.veralink/config.js \033[0m");
         process.exit();
     }
 
-    this.functions = require('./lib/functions.js')(this.HAPNode,Veraconfig);
+    this.functions = require('./lib/functions.js')(this.HAPNode, Veraconfig);
 }
 
-function loadconfig()
-{
+function loadconfig() {
     var fs = require('fs');
     home = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
     try {
-        fs.accessSync(home+'/.veralink', fs.F_OK);
+        fs.accessSync(home + '/.veralink', fs.F_OK);
 
         try {
-            fs.accessSync(home+'/.veralink/config.js', fs.F_OK);
+            fs.accessSync(home + '/.veralink/config.js', fs.F_OK);
             return require('./config.js');
-        } catch(e) {
+        } catch (e) {
             return {};
         }
     } catch (e) {
         try {
-            fs.mkdirSync(home+'/.veralink');
+            fs.mkdirSync(home + '/.veralink');
             return {};
-        } catch(e) {
-            if ( e.code != 'EEXIST' ) throw e;
+        } catch (e) {
+            if (e.code != 'EEXIST') throw e;
         }
     }
 }
 
-function merge_options(obj1,obj2)
-{
+function merge_options(obj1, obj2) {
     var obj3 = {};
     for (var attrname in obj1) { obj3[attrname] = obj1[attrname]; }
     for (var attrname in obj2) { obj3[attrname] = obj2[attrname]; }
